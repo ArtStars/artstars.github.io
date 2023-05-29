6909343b53f791d1f3f8fc178a8f92852753e181
CJ <83363242+LinuxGamer@users.noreply.github.com>
2023-05-29 10:45:31 +0100

    Update script.js

--- textedit/script.js
+++ textedit/script.js
@@ -1,11 +1,3 @@
-document.getElementById('boldBtn').addEventListener('click', function() {
-    document.execCommand('bold', false, null);
-});
-
-document.getElementById('italicBtn').addEventListener('click', function() {
-    document.execCommand('italic', false, null);
-});
-
-document.getElementById('underlineBtn').addEventListener('click', function() {
-    document.execCommand('underline', false, null);
+var simplemde = new SimpleMDE({
+    element: document.getElementById("editor"),
 });
